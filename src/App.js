03b8e7fbc364f94e355e7319e870a4511b05415e
LiteralMetaphorical <blesic.andrej@gmail.com>
2019-10-30 17:02:49 -0700

    test sockets

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { createChart } from 'lightweight-charts';
 import './App.css';
 
+const websocketUrl = 'ws://localhost:8000/charts';
+
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [chartRendered, setChartRendered] = useState(false);
+
+  useEffect(() => {
+    if (!chartRendered) {
+      const ws = new WebSocket(websocketUrl);
+      ws.onmessage = event => {
+        let chartData = JSON.parse(event.data);
+        chartData = chartData.chart;
+        // console.log(chartData);
+        for (const item in chartData) {
+          // chartData[item].time = chartData[item].time.substr(0, 10);
+        }
+        console.table(chartData);
+        if (chartData) {
+          const chart = createChart(document.getElementById('test'), { width: 600, height: 300 });
+          const lineSeries = chart.addLineSeries();
+          lineSeries.setData(chartData);
+        }
+      };
+      // lineSeries.setData([
+      //     { time: '2019-04-11', value: 80.01 },
+      //     { time: '2019-04-12', value: 96.63 },
+      //     { time: '2019-04-13', value: 76.64 },
+      //     { time: '2019-04-14', value: 81.89 },
+      //     { time: '2019-04-15', value: 74.43 },
+      //     { time: '2019-04-16', value: 80.01 },
+      //     { time: '2019-04-17', value: 96.63 },
+      //     { time: '2019-04-18', value: 76.64 },
+      //     { time: '2019-04-19', value: 81.89 },
+      //     { time: '2019-04-20', value: 74.43 },
+      // ]);
+      setChartRendered(true);
+    }
+  });
+
+  const increaseCount = () => {
+    setCount(count + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>You've pressed the button {count} times</p>
+      <button onClick={increaseCount}>+1</button>
+      <div id='test'></div>
     </div>
   );
 }
