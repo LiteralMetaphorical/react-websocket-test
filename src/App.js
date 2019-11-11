023c36a6c8134ee35b63b92fbcfdfbc7e70f48ae
LiteralMetaphorical <blesic.andrej@gmail.com>
2019-11-11 17:05:33 +0100

    added candlestick chart

--- src/App.js
+++ src/App.js
@@ -3,6 +3,14 @@ import { createChart } from 'lightweight-charts';
 import './App.css';
 
 // const websocketUrl = 'ws://192.168.0.180:8000/charts';
+const candleStickUrl = 'ws://localhost:8000/candela';
+
+const candleWs = new WebSocket(candleStickUrl);
+
+candleWs.onopen = () => {
+  console.log('opened')
+}
+
 const coinbaseWSUrl = 'wss://ws-feed.pro.coinbase.com';
 const currencyPairsUrl = 'https://api.pro.coinbase.com/products';
 const chartOptions = {
@@ -41,32 +49,11 @@ const chartOptions = {
   },
 };
 
-// const isDST = (d) => {
-//   let jan = new Date(d.getFullYear(), 0, 1).getTimezoneOffset();
-//   let jul = new Date(d.getFullYear(), 6, 1).getTimezoneOffset();
-//   console.log(Math.max(jan, jul) != d.getTimezoneOffset());
-// }
-
-// const cbws = new WebSocket(coinbaseWSUrl);
-//
-// cbws.onopen = () => {
-//   cbws.send(JSON.stringify(
-//     {
-//       "type": "subscribe",
-//       "product_ids": ["ETH-USD"],
-//       "channels": ["ticker"]
-//     }
-//   ));
-// }
-//
-// cbws.onmessage = event => {
-//   console.log(JSON.parse(event.data));
-// }
-
 function App() {
   return (
     <div>
       <LineChart />
+      <CandleChart />
     </div>
   );
 }
@@ -76,6 +63,8 @@ function LineChart() {
   const [currencyPair, setPair] = useState('BTC-USD');
   const [chartRendered, setRendered] = useState(false);
   const [currencyPairs, setPairs] = useState([]);
+  const [wsOpened, setWsOpened] = useState(false);
+  const [count, increaseCount] = useState(0);
 
   useEffect(() => {
     if (currencyPairs.length < 1) {
@@ -92,14 +81,15 @@ function LineChart() {
       .catch(error => console.log(error));
     }
     if (chartRendered) {
-      document.getElementById('test').firstChild.remove();
+      document.getElementById('line-chart').firstChild.remove();
     }
     let width = window.innerWidth;
     let height = window.innerHeight;
     const ws = new WebSocket(coinbaseWSUrl);
+    setWsOpened(true);
     const chart = createChart(
-      document.getElementById('test'),
-      { width: width, height: height - 100}
+      document.getElementById('line-chart'),
+      { width: width - 17, height: height - 100}
     );
     const areaSeries = chart.addAreaSeries({title: currencyPair});
     chart.applyOptions(chartOptions);
@@ -127,18 +117,75 @@ function LineChart() {
       chart.resize(height, width);
     });
     setRendered(true);
-  }, [currencyPair, chartRendered, currencyPairs]);
+  }, [currencyPair, chartRendered, currencyPairs, wsOpened]);
 
   return (
     <div>
-      <div id='test'></div>
+      <div id='line-chart'></div>
       <div>
         <select value={currencyPair} onChange={(event) => setPair(event.target.value)}>
          {currencyPairs.map((item, i) => <option key={`${item}${i}`} value={item}>{item}</option>)}
         </select>
+        <button onClick={() => increaseCount(count + 1)}>count +1</button>
+        <p>{count}</p>
       </div>
     </div>
   );
+}
+
+function CandleChart() {
+
+  const [chartRendered, setRendered] = useState(false);
+
+  useEffect(() => {
+    let candlestickSeries = undefined;
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    const chart = createChart(
+      document.getElementById('candle-chart'),
+      { width: width - 17, height: height - 100}
+    );
+    chart.applyOptions({
+      timeScale: {
+        rightOffset: 12,
+        barSpacing: 3,
+        fixLeftEdge: false,
+        lockVisibleTimeRangeOnResize: true,
+        rightBarStaysOnScroll: true,
+        borderVisible: false,
+        borderColor: '#fff000',
+        visible: true,
+        timeVisible: true,
+        secondsVisible: true,
+      },
+      layout: {
+        backgroundColor: '#282c34',
+        textColor: '#696969',
+        fontSize: 12,
+        fontFamily: 'Calibri',
+      }
+    })
+    candlestickSeries = chart.addCandlestickSeries({title: 'candle'});
+    candleWs.onmessage = (message) => {
+      let chartData = JSON.parse(message.data).candela;
+      if (chartData.length > 1) {
+        console.log(chartData);
+        candlestickSeries.setData(chartData);
+      } else if (chartData.length <= 1) {
+        console.log(chartData);
+        candlestickSeries.update(chartData[0]);
+      }
+    }
+    window.addEventListener('resize', () => {
+      width = window.innerWidth;
+      height = window.innerHeight;
+      chart.resize(height, width);
+    });
+  }, [chartRendered]);
+
+  return(
+    <div id='candle-chart'></div>
+  )
 }
 
 export default App;
@@ -153,4 +200,29 @@ export default App;
 // } else if (!chartData.chart) {
 //   chartData.time = Math.floor(chartData.time / 1000)
 //   areaSeries.update(chartData);
+// }
+
+
+///////////////////////////////////////
+
+// const isDST = (d) => {
+//   let jan = new Date(d.getFullYear(), 0, 1).getTimezoneOffset();
+//   let jul = new Date(d.getFullYear(), 6, 1).getTimezoneOffset();
+//   console.log(Math.max(jan, jul) != d.getTimezoneOffset());
+// }
+
+// const cbws = new WebSocket(coinbaseWSUrl);
+//
+// cbws.onopen = () => {
+//   cbws.send(JSON.stringify(
+//     {
+//       "type": "subscribe",
+//       "product_ids": ["ETH-USD"],
+//       "channels": ["ticker"]
+//     }
+//   ));
+// }
+//
+// cbws.onmessage = event => {
+//   console.log(JSON.parse(event.data));
 // }
